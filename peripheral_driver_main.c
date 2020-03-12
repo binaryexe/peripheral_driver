0213a42660836afc6c0e7bb22c30cf32986bf662
binary exe <exebinary@gmail.com>
2020-03-12 14:24:56 +0530

    adding file
    Signed-off-by: binary exe <exebinary@gmail.com>

--- peripheral_driver_main.c
+++ peripheral_driver_main.c
@@ -14,12 +14,12 @@ static int __init pdd_init(void)
 
 static void __exit pdd_exit(void)
 {
-    printk(KERN_INFO "PDD Exit Called")
+    printk(KERN_INFO "PDD Exit Called");
 }
 
 module_init(pdd_init);
 module_exit(pdd_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("exebinary@gmail.com")
-MODULE_DESCRIPTION("For self-learning LDD")
+MODULE_AUTHOR("exebinary@gmail.com");
+MODULE_DESCRIPTION("For self-learning LDD");
